dd43d8d84845ec7f99d8cbf86a27d4999393a971
Chirag Ramachandraiah <chirag_ramachandraiah@external.mckinsey.com>
2019-01-22 12:16:21 +0530

    made some changes

--- assignment1/assignment1.test.js
+++ assignment1/assignment1.test.js
@@ -11,21 +11,25 @@ var tio=[
 var op=[90,30,16,300,0];
 
 describe('Testing',() => {
+    
+    it('Checking if initial values are set correctly',() => {
+        expect(program.tally.frames).toEqual([]);
+        expect(program.tally.points).toEqual(0);
+    });
+    
+    it('Checking if pins adds to list',() => {
+        program.pins(2);
+        program.pins(3);
+        expect(program.tally.frames).toEqual([2,3]);
+        expect(program.tally.frames).not.toEqual([]);
+    });
+
     test('Checking if correct score is returned',() => {
         for(var i=0;i<tio.length;i++){
         expect(program.runner(tio[i])).toEqual(op[i]);
         expect(program.tally.frames).toEqual(tio[i]);
         expect(program.tally.points).toEqual(op[i]);
     }
-    });
-
-    it('Checking if pins adds to list',() => {
-    expect(program.score()).toEqual(0);
-    })
-
-    it('Checking if initial values are set correctly',() => {
-        // expect(program.tally.frames).toEqual([]);
-        expect(program.tally.points).toEqual(0);
     });
 
 });
